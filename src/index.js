e817626fda0e2289839a5d250e837f99e0347d0f
YuraWin <yurawin@ukr.net>
2020-11-01 22:37:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './styles.css';
+
+// task 1 
+
+const delay = ms => {
+
+};
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+delay(2000).then(logger);
+delay(1000).then(logger);
+delay(1500).then(logger);
